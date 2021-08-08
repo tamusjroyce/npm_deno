e8c5da22c4f037f2c19a46a743880beddea5f72d
TamusJRoyce <tamusjroyce@hotmail.com>
2021-08-08 19:55:33 -0400

    posix fix

--- denojs.js
+++ denojs.js
@@ -3,7 +3,11 @@ const { spawnSync } = require('child_process');
 function deno(args) {
   let exitCode = 0;
   try {
-    exitCode = spawnSync('deno', args, { stdio: 'inherit', stdout: 'inherit', stderr: 'inherit' }).status || 0;
+    if (process.platform === 'win32') {
+      exitCode = spawnSync('deno.exe', args, { stdio: 'inherit', stdout: 'inherit', stderr: 'inherit' }).status || 0;
+    } else {
+      exitCode = spawnSync('./deno', args, { stdio: 'inherit', stdout: 'inherit', stderr: 'inherit' }).status || 0;
+    }
   } catch(ex) {
     console.log(ex);
     exitCode = -1;
