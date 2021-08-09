d4da6678efd7e9321d8cbd400b892a930fa2a9af
TamusJRoyce <tamusjroyce@hotmail.com>
2021-08-08 20:02:44 -0400

    refactor install script

--- denoinstall.js
+++ denoinstall.js
@@ -1,17 +1,28 @@
 const { execSync } = require('child_process');
 const fs = require('fs');
 
-function install_deno() {
-  if (fs.statSync('deno', { throwIfNoEntry: false })?.isFile() || fs.statSync('deno.exe', { throwIfNoEntry: false })?.isFile() ||
-      fs.statSync('./bin/deno', { throwIfNoEntry: false })?.isFile() || fs.statSync('./bin/deno.exe', { throwIfNoEntry: false })?.isFile()) {
-    return 0;
-  }
-  if (process.platform === 'win32') {
-    execSync(`PowerShell -Command \"$env:DENO_INSTALL='${process.cwd()}'; iwr https://deno.land/x/install/install.ps1 -useb | iex\"`);
-  } else {
-    execSync(`export DENO_INSTALL="${process.cwd()}" && curl -fsSL https://deno.land/x/install/install.sh | sh || wget -o - https://deno.land/x/install/install.sh | sh`);
-  }
-  return 1;
+function install_win32_deno() {
+  execSync(`PowerShell -Command \"$env:DENO_INSTALL='${process.cwd()}'; iwr https://deno.land/x/install/install.ps1 -useb | iex\"`);
 }
+
+function install_posix_deno() {
+  execSync(`export DENO_INSTALL="${process.cwd()}" && curl -fsSL https://deno.land/x/install/install.sh | sh || wget -o - https://deno.land/x/install/install.sh | sh`);
+}
+
+function install_deno() {
+  if (process.platform !== 'win32') {
+    if (fs.statSync('deno', { throwIfNoEntry: false })?.isFile() || fs.statSync('./bin/deno', { throwIfNoEntry: false })?.isFile()) {
+      return;
+    } else {
+      install_posix_deno();
+    }
+  } else {
+    if (process.platform === 'win32' && fs.statSync('deno.exe', { throwIfNoEntry: false })?.isFile() || fs.statSync('./bin/deno.exe', { throwIfNoEntry: false })?.isFile()) {
+      return;
+    } else {
+      install_win32_deno();
+    }
+  }
+}
+
 install_deno();
-module.exports = install_deno;
