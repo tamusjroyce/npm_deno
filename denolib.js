9ed122ad0a68b5aa465a2cd68fb6ba59190bdc5b
TamusJRoyce <tamusjroyce@hotmail.com>
2021-08-12 08:05:57 -0400

    additional changes for updating package name

--- denolib.js
+++ denolib.js
@@ -4,7 +4,7 @@ function deno(args) {
   let exitCode = 0;
   try {
     const denoExe = process.platform === 'win32' ? 'deno.exe' : 'deno';
-    exitCode = spawnSync(`./node_modules/npm_deno/bin/${denoExe}`, args, { stdio: 'inherit', stdout: 'inherit', stderr: 'inherit' }).status || 0;
+    exitCode = spawnSync(`./node_modules/@tamusjroyce/npm_deno/bin/${denoExe}`, args, { stdio: 'inherit', stdout: 'inherit', stderr: 'inherit' }).status || 0;
   } catch(ex) {
     console.log(ex);
     exitCode = -1;
